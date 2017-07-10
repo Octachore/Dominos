d75f86860b9997211ddf624682f343aaefb7872b
Octachore <octachore@outlook.com>
2017-07-10 10:41:31 +0200

    move web logic out of game logic

--- Backend/Backend/GameLogic.fs
+++ Backend/Backend/GameLogic.fs
@@ -1,21 +1,17 @@
 ﻿module GameLogic
 
-open Suave.Successful
-open Suave.RequestErrors
-
-
 type Domino = {v1:int; v2:int}
 
 type Position = {x:int; y:int}
 
 type ActionResult =
-    | Success
-    | Failure
+    | Success of string
+    | Failure of string
 
 let play name action (domino : Domino) (position : Position) =
     match action with
-    | "draw" -> OK (sprintf "%s draws a new domino from the deck" name)
-    | "place" -> OK (sprintf "%s places a domino of value %i:%i on the board at position %i:%i" name domino.v1 domino.v2 position.x position.y)
-    | "quit" -> OK (sprintf "%s quits the game" name)
-    | "win" -> OK (sprintf "%s wins the game!" name)
-    | _ -> BAD_REQUEST "Unknown action"
+    | "draw" -> Success (sprintf "%s draws a new domino from the deck" name)
+    | "place" -> Success (sprintf "%s places a domino of value %i:%i on the board at position %i:%i" name domino.v1 domino.v2 position.x position.y)
+    | "quit" -> Success (sprintf "%s quits the game" name)
+    | "win" -> Success (sprintf "%s wins the game!" name)
+    | _ -> Failure "Unknown action"
--- Backend/Backend/Routes.fs
+++ Backend/Backend/Routes.fs
@@ -7,7 +7,6 @@ open Suave.Successful
 open Suave.RequestErrors
 open MetaGameLogic
 open GameLogic
-open System
 
 let route_join =
     request (fun r ->
@@ -27,12 +26,20 @@ let route_start_game =
         | true -> start_game
         | _ -> handle_no_game_available true)
 
+let handle_play result = 
+    match result with
+        | Success s -> OK s
+        | Failure f -> BAD_REQUEST f
+
 let route_play_post =
     request(fun r ->
         match r.formData "name", r.formData "action", r.formData "domino-v1", r.formData "domino-v2", r.formData "position-x", r.formData "position-y" with
-        | Choice1Of2 name, Choice1Of2 action, Choice2Of2 _, Choice2Of2 _, Choice2Of2 _, Choice2Of2 _ -> play name action {v1= -1; v2= -1} {x= -1; y= -1}
-        | Choice1Of2 name, Choice1Of2 action,  Choice1Of2 v1 , Choice1Of2 v2, Choice1Of2 x, Choice1Of2 y ->  play name action {v1= v1 |> int; v2= v2 |> int} {x= x |> int; y= y |> int}
+        | Choice1Of2 name, Choice1Of2 action, Choice2Of2 _, Choice2Of2 _, Choice2Of2 _, Choice2Of2 _ 
+            -> handle_play (play name action {v1= -1; v2= -1} {x= -1; y= -1})   
+        | Choice1Of2 name, Choice1Of2 action,  Choice1Of2 v1 , Choice1Of2 v2, Choice1Of2 x, Choice1Of2 y 
+            -> handle_play (play name action {v1= v1 |> int; v2= v2 |> int} {x= x |> int; y= y |> int} )
         | _ -> BAD_REQUEST "error")
+
 
 let routes = [ GET >=> choose
                  [ path "/hello" >=> OK "Hello GET"
